d3d2eb1bfad6bc507a3781d592316b9b77b39e9c
Erick Rivera <rivera.erick95@gmail.com>
2023-03-19 10:42:02 -0400

    Initialized translator and test cases

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,14 @@
+import unittest
+from . import TranslatorManager
+
+class testTranslator(unittest.TestCase):
+    def __init__(self):
+        translator = TranslatorManager()
+
+
+    def testFrenchToEnglish(self):
+        self.assertEqual(translator)
+
+
+    def testEnglishToFrench(self):
+        pass
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,30 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+class TranslatorManager:    
+    def __init__(self):
+        load_dotenv()
+        apikey = os.environ['apikey']
+        url = os.environ['url']
+        IAM_auth = IAMAuthenticator(apikey) 
+        self.lang_translator = LanguageTranslatorV3("2018-05-01", IAM_auth)
+        self.lang_translator.set_service_url(url)
+
+    def englishToFrench(self, englishText):
+        original = "en"
+        translated = "fr"
+        model_id = f'{original}-{translated}'
+        response = self.lang_translator.translate(englishText, model_id = model_id, 
+            source = original, target = translated)
+
+    def frenchToEnglish(self, frenchText):
+        original = "fe"
+        translated = "en"
+        model_id = f'{original}-{translated}'
+        response = self.lang_translator.translate(frenchText, model_id = model_id, 
+            source = original, target = translated)
+        if response.get_status_code != 200:
+            print("Error, ")
