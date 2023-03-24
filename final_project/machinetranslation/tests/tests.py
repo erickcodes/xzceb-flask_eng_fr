890a4d67a3cb2315c200777e642e7b09bf515d66
erickcodes <rivera.erick95@gmail.com>
2023-03-24 09:38:44 -0400

    Final changes. Docstring and package file management.

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,23 @@
+import unittest
+from ..translator import Translator
+
+class TestTranslator(unittest.TestCase):
+    def setUp(self):
+        self.watson = Translator()
+        super().setUp()
+
+    def tests_en2fr_None(self):
+        self.assertIsNone(self.watson.englishToFrench(None))
+
+    def tests_fr2en_None(self):
+        self.assertIsNone(self.watson.frenchToEnglish(None))
+
+    def tests_en2fr_Hello(self):
+        self.assertEqual(self.watson.englishToFrench("Hello"), "Bonjour")
+
+    def tests_fr2en_Bonjour(self):
+        self.assertEqual(self.watson.frenchToEnglish("Bonjour"), "Hello")
+
+if __name__ == '__main__':
+    # Resource Warning
+    unittest.main(warnings='ignore')
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -4,38 +4,66 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import os
 from dotenv import load_dotenv
 
-class Translator:    
+class Translator:
+    """
+    A class represents our translation service that utilizes IBM's AI Watson.
+    ...
+    Attributes
+    ----------
+    lang_translator : LanguageTranslatorV3
+        
+    Methods
+    -------
+    __init__(self) : Translator
+        initial authenication and watson api service
+    translate(self, text, original, translated) : DetailedResponse
+        translation of the text from orignal langauge to the translated langauge
+    englishToFrench(self, englishText) : str
+        translates englishText into French
+    frenchToEnglish(self, frenchText) : str
+        translates frenchText into English
+    """
+
     def __init__(self):
         load_dotenv()
         apikey = os.environ['apikey']
         url = os.environ['url']
-        IAM_auth = IAMAuthenticator(apikey) 
-        self.lang_translator = LanguageTranslatorV3("2018-05-01", IAM_auth)
+        iam_auth = IAMAuthenticator(apikey)
+        self.lang_translator = LanguageTranslatorV3("2018-05-01", iam_auth)
         self.lang_translator.set_service_url(url)
 
-    def translate(self, text, original, translated):        
+    def translate(self, text, original, translated):
+        """
+        return DetailedResponse, text from original langauge to translated langauge
+        """
         model_id = f'{original}-{translated}'
-        return self.lang_translator.translate(text, model_id = model_id, 
+        return self.lang_translator.translate(text, model_id = model_id,
             source = original, target = translated)
 
     def englishToFrench(self, englishText):
+        """
+        return French translation of englishText
+        """
         if englishText is None:
-            return
+            return None
         original = "en"
         translated = "fr"
         response = self.translate(englishText, original, translated)
         if response.get_status_code() != 200:
             print(f'Error! Status code: {response.get_status_code()}')
-            return
+            return None
         return response.get_result()["translations"][0]["translation"]
 
     def frenchToEnglish(self, frenchText):
+        """
+        return English translation of frenchText
+        """
         if frenchText is None:
-            return
+            return None
         original = "fr"
         translated = "en"
         response = self.translate(frenchText, original, translated)
         if response.get_status_code() != 200:
             print(f'Error! Status code: {response.get_status_code()}')
-            return
+            return None
         return response.get_result()["translations"][0]["translation"]
